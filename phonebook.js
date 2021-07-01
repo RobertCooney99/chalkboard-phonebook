0c29605b460a55319b873a49883d7b909ac80e1b
Robert Cooney <47756961+RobertCooney99@users.noreply.github.com>
2021-07-01 15:39:26 +0100

    Added API token using JSONWebToken package
    Route /contacts/login takes a username & password combo. If correct the API returns an API token

--- phonebook.js
+++ phonebook.js
@@ -2,6 +2,11 @@ const mysql = require('mysql');
 const express = require('express');
 const bodyparser = require('body-parser');
 const { query } = require('express');
+const jwt = require('jsonwebtoken');
+
+// Secret key for API
+// In production this would be kept hidden and secure
+const api_secret_key = 'secretkeystring';
 
 var app = express();
 app.use(bodyparser.json());
@@ -28,3 +33,28 @@ mysqlConnection.connect((err) => {
 // Begin listening for requests on port 8080
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`App listening on port: ${port}...`));
+
+// Route to login to API
+// In this application there is only one account
+// Username: Admin, password: password
+// When these details are inputted an API token is returned
+app.post('/contacts/login', (req, res) => {
+    let login_details = req.body;
+    if (login_details.username == "Admin" && login_details.password == "password") {
+        // The correct login details have been added
+        // Create a JWT API Token using the account username, password and API secret key
+        // Can set an expiry time of 2 hours by including {expiresIn: '2h'}
+        jwt.sign({username: login_details.username, password: login_details.password}, api_secret_key, (err, token) => {
+            if (!err) {
+                res.json({
+                    token
+                });
+            } else {
+                console.log(err);
+            }
+        });
+    } else {
+        // The inputted username & password combination is incorrect
+        res.send("Incorrect login...");
+    }
+});
