84d06d1aa50942afb7404734fbf2a9d8bcfd5af1
Robert Cooney <47756961+RobertCooney99@users.noreply.github.com>
2021-07-01 15:27:48 +0100

    Add files via upload
    Query to populate the phonebook 'contacts' table with 6 random contacts.

---
+++ sql/populate-table.sql
@@ -0,0 +1,8 @@
+INSERT INTO contacts (first_name, last_name, work_phone, home_phone, mobile_phone, other_phone, email, mailing_address)
+VALUES
+	("Ronnie", "Jones", "12345 123456", "0121 123 2000", NULL, NULL, "R.Jones@gmail.com", "1 Meddow Street, Birmingham, B10 2MK"),
+    ("Carol", "Armfield", "21912 283328", "2901 283 4823", "29382 094300", NULL, "carolA@email.com", "Flat 7 Oak Tree Court, Newcastle, N22 1JS"),
+    ("Simon", "Petrol", "25844 492839", "9219 382 3849", "94823 483929", "90439 393200", "simon1972@petrol.co.uk", "43 Helm Vale, Manchester, M20 1EB"),
+    ("Natasha", "Dale", NULL, NULL, "07349 920920", NULL, "Natasha1000@gmail.co.uk", NULL);
+    ("Mary", "Stevenson", "05843 556123", NULL, NULL, NULL, "MStevenson@mary.com", "Flat 4 Orange Tower, Glasgow, G13 3PO");
+    ("Abdullah", "Kumal", NULL, "0181 732 4832", NULL, NULL, "absK@gmail.com", "Streamview House, Harrow Lane, Liverpool, L27 8EW");
